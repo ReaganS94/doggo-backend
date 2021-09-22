426f65110a4e937a23bbd310ca8614471976eb54
ReaganS <reagansasan@gmail.com>
2021-09-22 16:28:27 +0200

    implemented password hashing

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const { Schema } = mongoose;
 
@@ -67,6 +68,29 @@ const UserSchema = new Schema({
     type: Schema.ObjectId,
     ref: "dog",
   },
+});
+
+UserSchema.pre("save", function (next) {
+  const user = this;
+
+  if (this.isModified("password") || this.isNew) {
+    bcrypt.genSalt(10, function (saltError, salt) {
+      if (saltError) {
+        return next(saltError);
+      } else {
+        bcrypt.hash(user.password, salt, function (hashError, hash) {
+          if (hashError) {
+            return next(hashError);
+          }
+
+          user.password = hash;
+          next();
+        });
+      }
+    });
+  } else {
+    return next();
+  }
 });
 
 module.exports = mongoose.model("User", UserSchema, "users");
