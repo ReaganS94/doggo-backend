948dea4b727c70d049984ae6f571d33e8d471fae
Reagan <83071617+ReaganS94@users.noreply.github.com>
2022-09-19 11:06:02 +0200

    Update User.js

--- models/User.js
+++ models/User.js
@@ -79,27 +79,4 @@ const UserSchema = new Schema({
   },
 });
 
-UserSchema.pre("save", function (next) {
-  const user = this;
-
-  if (this.isModified("password") || this.isNew) {
-    bcrypt.genSalt(10, function (saltError, salt) {
-      if (saltError) {
-        return next(saltError);
-      } else {
-        bcrypt.hash(user.password, salt, function (hashError, hash) {
-          if (hashError) {
-            return next(hashError);
-          }
-
-          user.password = hash;
-          next();
-        });
-      }
-    });
-  } else {
-    return next();
-  }
-});
-
 module.exports = mongoose.model("User", UserSchema, "users");
